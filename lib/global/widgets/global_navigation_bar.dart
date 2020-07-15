2bb1c256c8d6f74f85b5a821912c866cd6ced8de
Ifavhoney <nuthsaid@outlook.com>
2020-07-15 07:31:07 -0400

    completes bottom navigation bar

---
+++ lib/global/widgets/global_navigation_bar.dart
@@ -0,0 +1,141 @@
+import 'package:buddy/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class GlobalNavigationBar extends StatefulWidget {
+  GlobalNavigationBar({Key key}) : super(key: key);
+
+  @override
+  _GlobalNavigationBarState createState() => _GlobalNavigationBarState();
+}
+
+class _GlobalNavigationBarState extends State<GlobalNavigationBar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 120, color: Colors.white, child: _navigationBar());
+  }
+
+//Widgets
+  Widget _navigationBar() => Stack(
+        children: <Widget>[
+          Positioned(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade400,
+                      blurRadius: 10,
+                      spreadRadius: 2)
+                ]),
+                child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20, top: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        _icon(Icons.settings, 0),
+                        Container(),
+                        Container(),
+                        Container(),
+                        Container(),
+                        _icon(Icons.people, 2),
+                        _icon(Icons.today, 3)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          _search(),
+          _buddy()
+        ],
+      );
+
+  Widget _icon(IconData icon, int index) => Column(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              icon,
+              color: _currentIndex == index
+                  ? AppTheme.bottomNavigationBarIcons["active"]
+                  : AppTheme.bottomNavigationBarIcons["inactive"],
+              size: 35,
+            ),
+            onPressed: () {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+          _dot(index),
+        ],
+      );
+
+  Widget _search() => Positioned(
+        child: GestureDetector(
+            onTap: () {
+              setState(() {
+                _currentIndex = 1;
+              });
+            },
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 40),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentIndex == 1
+                              ? AppTheme.bottomNavigationBarIcons["active"]
+                              : AppTheme.bottomNavigationBarIcons["search"],
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 8,
+                                spreadRadius: -2)
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Icon(
+                          Icons.search,
+                          size: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+      );
+
+  Widget _buddy() => Positioned(
+          child: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Text("Buddy",
+              style: AppTheme.nexa.bodyText1.apply(color: Color(0xFFB9D1FF))),
+        ),
+      ));
+
+  Widget _dot(int index) => _currentIndex == index
+      ? Transform(
+          transform: Matrix4.translationValues(3, -8.5, 0.0),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppTheme.bottomNavigationBarIcons["active"],
+            ),
+            child: Text("  "),
+          ),
+        )
+      : Container();
+}
--- lib/searching/screens/search_screen.dart
+++ lib/searching/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:buddy/global/widgets/global_navigation_bar.dart';
 import 'package:buddy/theme/colors/icon_colors.dart';
 import 'package:buddy/theme/theme.dart';
 import 'package:flutter/material.dart';
@@ -10,169 +11,26 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  int _currentIndex = 0;
   final List<Widget> _children = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            Icons.mic,
-            size: 100,
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.mic,
+              size: 100,
+            ),
           ),
         ),
-      ),
-      backgroundColor: Colors.white,
-      bottomNavigationBar: Container(
-        height: 120,
-        color: Colors.white,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 90,
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.shade400,
-                        blurRadius: 10,
-                        spreadRadius: 2)
-                  ]),
-                  child: Container(
-                    color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(
-                              Icons.settings,
-                              color: Color(0xFF6F8BA4),
-                              size: 40,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _currentIndex = 0;
-                              });
-                            },
-                          ),
-                          Container(),
-                          Container(),
-                          Container(),
-                          Container(),
-                          IconButton(
-                            icon: Icon(
-                              Icons.people,
-                              color: Color(0xFF6F8BA4),
-                              size: 40,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _currentIndex = 2;
-                              });
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.today,
-                              color: Color(0xFF6F8BA4),
-                              size: 40,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _currentIndex = 3;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _currentIndex = 1;
-                  });
-                },
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 40),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: AppTheme.bottomNavigationBarIcons["search"],
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black,
-                                blurRadius: 8,
-                                spreadRadius: -2)
-                          ]),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Icon(
-                          Icons.search,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-                child: Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Text("Buddy",
-                    style: AppTheme.nexa.bodyText1
-                        .apply(color: Color(0xFFB9D1FF))),
-              ),
-            )),
-
-            /*
-                Icon( 
-                  Icons.search,
-                  color: Color(0xFF6F8BA4),
-                  size: 35,
-                )
-                */
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        bottomNavigationBar: GlobalNavigationBar());
   }
 
-  /*
-BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex:
-            _currentIndex, // this will be set when a new tab is tapped
-
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("")),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text(""))
-        ],
-      ),
-  */
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
+  Widget item(Icon icon) {}
+  Widget search(Icon icon) {}
 }
