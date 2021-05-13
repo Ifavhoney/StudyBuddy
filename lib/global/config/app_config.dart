7cb757e47e22791237e5039f7394fe001186b123
jason-nguessan <nuthsaid@outlook.com>
2021-05-13 06:56:05 -0400

    config

---
+++ lib/global/config/app_config.dart
@@ -0,0 +1,13 @@
+class AppConfig {
+  //General
+  String appName = "Bored";
+  String termsAndConditions = "";
+  String websiteUrl = "";
+
+  //Social Media
+  String facebookUrl = "";
+  String twitterUrl = "";
+  String youtubeUrl = "";
+  String instagramUrl = "";
+  String gitHubUrl = "";
+}
